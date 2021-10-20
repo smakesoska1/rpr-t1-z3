aba0ae0c4e0c77bd435a59a0111b6f2f160f9111
smakesoska1 <smakesoska1@etf.unsa.ba>
2021-10-20 16:51:58 +0200

    Initial commit

---
+++ src/ba/unsa/etf/rpr/Sat.java
@@ -0,0 +1,62 @@
+package ba.unsa.etf.rpr;
+
+public class Sat {
+        private int sati,minute,sekunde;
+
+        public Sat(int sati, int minute, int sekunde) {
+            postavi(sati,minute,sekunde);
+        }
+        public void postavi(int sati, int minute, int sekunde) {
+            this.sati=sati;
+            this.minute=minute;
+            this.sekunde=sekunde;
+        }
+        public void sljedeci() {
+            sekunde++;
+            if (sekunde==60) {
+                sekunde=0;
+                minute++;
+            }
+            if (minute==60) {
+                minute=0;
+                sati++;
+            }
+            if (sati==24) sati=0;
+        }
+        public void prethodni() {
+            sekunde--;
+            if (sekunde==-1) {
+                sekunde=59;
+                minute--;
+            }
+            if (minute==-1) {
+                minute=59;
+                sati--;
+            }
+            if (sati==-1) sati=23;
+        }
+        void pomjeriZa(int pomak) {
+            if (pomak > 0) {
+                for (int i = 0; i < pomak; i++)
+                    sljedeci();
+
+            } else {
+                for (int i = 0; i < -pomak; i++)
+                    prethodni();
+            }
+        }
+        public int dajSate() {
+            return sati;
+        }
+        int dajMinute()
+        { return minute;
+        }
+        int dajSekunde(){
+            return sekunde;
+        }
+        public void ispisi()  {
+            System.out.println(sati + ":" + minute + ":" + sekunde);
+        }
+
+
+}
